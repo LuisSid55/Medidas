26e2a9a2dd356f32cac518a710551c2779b3d88a
LuisSid55 <luiscitty4@gmail.com>
2021-09-25 18:22:46 -0300

    Medidas

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cm = 100, metros, km = 1000, cmValor, kmValor;
+            Console.WriteLine("\n---Medidas---");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("\nDigite em metros: ");
+            Console.ResetColor();
+            metros = Convert.ToDouble(Console.ReadLine());
+
+            cmValor = metros * cm;
+            kmValor = metros / km;
+            Console.WriteLine("\n---Resultado---");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\n {cmValor} cm\n {metros} m\n {kmValor} km\n");
+            Console.ResetColor();
+        }
+    }
+}
